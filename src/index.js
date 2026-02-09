86c5b9b3a9e5934bbac4c11f35b5ffbef24f9e21
Anderson Lopez <52222621+TheGodFatherpte@users.noreply.github.com>
2026-02-09 00:42:44 -0500

    implement matches feature for managing keeper  matches including POST and GET routes with Zod validation schemas, utility functions to cumpute and  sync match status

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-
-
 import express from 'express';
-
+import { matchRouter } from './routes/matches.js';
 const app = express();
 const port = 8000;
 
@@ -10,6 +8,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello from Express server!');
 });
+
+app.use('/matches', matchRouter)
 
 app.listen(port, () => {
 
